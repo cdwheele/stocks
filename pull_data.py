233083044c6e26bdea9ea6298d569a1386d10300
cdwheele <camdwheeler@gmail.com>
2020-10-10 12:16:19 -0700

    pull data baseline

---
+++ pull_data.py
@@ -0,0 +1,10 @@
+from polygon import RESTClient
+# Using env stonks
+
+# create a list of stocks to watch
+
+def main():
+    key = 'pbY1RM6pzUSJr4dxTJ0oo3SpZSw7ZxYq'
+
+    with RESTClient(key) as client:
+        response = client.
