dd918eb5187433ae9bb0eb71824305982df3a0e9
cdwheele <camdwheeler@gmail.com>
2020-10-10 14:03:19 -0700

    Adding getClose to get closing data for stocks

--- rest_data.py
+++ rest_data.py
@@ -10,10 +10,18 @@ class StockFinancials:
 def getFinancials(client, ticker):
     response = client.reference_stock_financials(ticker)
     PTE = response.results[0]['priceToEarningsRatio']
-    EBT = response.results[0]['earningsBeforeInterestTaxesDepreciationAmortizationUSD']
+    EBT = response.results[0]['enterpriseValueOverEBITDA']
+    #EBT = response.results[0]['earningsBeforeInterestTaxesDepreciationAmortizationUSD']
     reportPeriod = response.results[0]['reportPeriod']
 
     return StockFinancials(ticker, PTE, EBT, reportPeriod)
+
+def getClose(client, ticker):
+    response = client.stocks_equities_previous_close(ticker)
+    previous_close = response.results[0]['c']
+    change = response.results[0]['c'] - response.results[0]['o']
+    print(change)
+
 
 def main():
     key = "pbY1RM6pzUSJr4dxTJ0oo3SpZSw7ZxYq"
@@ -24,8 +32,12 @@ def main():
     # price to earnings ratio priceToEarningsRatio
     # Earnings before earningsBeforeInterestTaxesDepreciationAmortizationUSD
     # stock financials
+
+    # List of functions found here:
+    # https://github.com/polygon-io/client-python/blob/master/polygon/rest/client.py
     with RESTClient(key) as client:
         apple = getFinancials(client, "AAPL")
+        getClose(client, "AAPL")
         print(apple.EBT)
 
         amd = getFinancials(client, "AMD")
